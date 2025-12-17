197bf32d22829add0111de167ac618bc51bc8d60
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-17 15:42:11 +0530

    Add files via upload

---
+++ src/ex_01_Java_Basics/Lab002_Name.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Name {
+    public static void main(String[] args){
+        System.out.print("hi soujanya-->");
+        System.out.println(" how r u");
+
+    }
+}
