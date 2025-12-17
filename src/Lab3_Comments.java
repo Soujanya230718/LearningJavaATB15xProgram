c0d3c8ff1ea47484eb217ec870f0620d7a8f79f4
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-17 18:40:31 +0530

    comments prog

---
+++ src/Lab3_Comments.java
@@ -0,0 +1,13 @@
+public class Lab3_Comments {
+    public static void main(String[] args) {
+
+        // this code is not execute
+        /* yfhjkhfbchj,dbfwj
+        hsbfjhdbjhdshb
+        nvbjbvjvj
+         */
+        System.out.println("commets");
+
+
+    }
+}
