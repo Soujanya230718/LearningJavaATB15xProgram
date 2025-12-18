e595b674d5938bbe65b4855026552f5e3767dc89
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 17:17:57 +0530

    variable 2 prog

---
+++ src/ex_02_Java_Basics/Variable2.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        //int enum= 47; //enum is non-primitive data type
+        int Enum=65;
+        System.out.println(Enum);
+        int $r= 46;
+        int euyrhjsd_3274hdj= 59494;
+
+    }
+}
