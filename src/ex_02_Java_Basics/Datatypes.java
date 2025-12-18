10f87abd523eb76b1c9ba3d7ee0a53797d5f0b52
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 17:28:26 +0530

    datatype values

---
+++ src/ex_02_Java_Basics/Datatypes.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Datatypes {
+    public static void main(String[] args) {
+
+        byte b = 127;
+        short s = 127;
+        int i = 127;
+        long l = 1278948839784L;  // should add 'L' in the last
+        float f = 34.8f;          // add 'f' in the last
+        double d = 12773893.3889983;
+        char c = 127;
+        char c1= '2';
+        char c2= 'A';
+    }
+}
