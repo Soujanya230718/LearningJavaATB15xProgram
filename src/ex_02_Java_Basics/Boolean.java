57ba7b38b67e27cff739babfec17628982b55856
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 17:22:37 +0530

    boolean eg:

---
+++ src/ex_02_Java_Basics/Boolean.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean tyeibfjfjfn = true;
+        boolean iudhfidiej= false;
+
+
+    }
+}
