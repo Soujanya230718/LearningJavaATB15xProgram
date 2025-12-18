d780fac6f22878fa1277394812dea8fd539577a8
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 16:31:24 +0530

    Basic Mathematics

---
+++ src/ex_02_Java_Basics/DivisionPro.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class DivisionPro {
+    public static void main(String[] args) {
+        System.out.println("10/0");
+        // System.out.println(100/0);
+       // System.out.println(10/0);
+        System.out.println(0/10);
+        System.out.println(10/1000);
+
+    }
+}
