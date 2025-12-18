2c6a0b3a5af3d2798205aa7c332cf9565702044b
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 17:07:08 +0530

    variables and values

---
+++ src/ex_02_Java_Basics/Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a= 10;
+        int a1= 20;
+        a= 66;
+        System.out.println(a); // newly updated value will print ignores the 1st value
+    }
+}
