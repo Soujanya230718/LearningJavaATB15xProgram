e886d821cc4ba3c4e60f695e126ce3fec4970a5d
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 15:00:27 +0530

    identifiers

---
+++ src/ex_02_Java_Basics/Identifiers.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        System.out.print("identifiers ");
+        // package, public, static, void, class
+
+    }
+}
