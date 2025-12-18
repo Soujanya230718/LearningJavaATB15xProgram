fd0e6dc2b1f6779913fc9a474f36661d100e108b
Soujanya230718 <soujanyareddy185@gmail.com>
2025-12-18 18:11:47 +0530

    9 table using printf and %n (new line)

--- src/ex_02_Java_Basics/TableUsingPrintF.java
+++ src/ex_02_Java_Basics/TableUsingPrintF.java
@@ -8,22 +8,14 @@ public class TableUsingPrintF {
         System.out.println("9 table is:");
         System.out.printf("%d*1=%d", table,table*1);
         System.out.println();
-        System.out.printf("%d*2=%d", table,table*2);
-        System.out.println();
-        System.out.printf("%d*3=%d", table,table*3);
-        System.out.println();
-        System.out.printf("%d*4=%d", table,table*4);
-        System.out.println();
-        System.out.printf("%d*5=%d", table,table*5);
-        System.out.println();
-        System.out.printf("%d*6=%d", table,table*6);
-        System.out.println();
-        System.out.printf("%d*7=%d", table,table*7);
-        System.out.println();
-        System.out.printf("%d*8=%d", table,table*8);
-        System.out.println();
-        System.out.printf("%d*9=%d", table,table*9);
-        System.out.println();
+        System.out.printf("%d*2=%d%n", table,table*2);
+        System.out.printf("%d*3=%d%n", table,table*3);
+        System.out.printf("%d*4=%d%n", table,table*4);
+        System.out.printf("%d*5=%d%n", table,table*5);
+        System.out.printf("%d*6=%d%n", table,table*6);
+        System.out.printf("%d*7=%d%n", table,table*7);
+        System.out.printf("%d*8=%d%n", table,table*8);
+        System.out.printf("%d*9=%d%n", table,table*9);
         System.out.printf("%d*10=%d", table,table*10);
 
     }
